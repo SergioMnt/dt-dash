9bf00d37ae765fadd95ede86456537cddec8eafd
SergioMnt <sergio.montemayorios@gmail.com>
2022-10-22 22:26:19 -0500

    Initializing repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import './App.scss';
+import { Navbar, SideBar } from "./Components/Navs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+	return (
+		<Fragment>
+			<h1>Hola</h1>
+			<img className='profile-pic' src={`https://avatars.dicebear.com/api/human/${Math.random()}.svg`} alt="" />
+		</Fragment>
+	)
 }
-
-export default App;
---
+++ src/Components/Navs.tsx
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+
+export const Navbar = () => {
+    return (
+        <Fragment>
+            <h1>This is the Navbar</h1>
+        </Fragment>
+    )
+}
+
+export const SideBar = () => {
+    return (
+        <Fragment>
+            <h1>This is the SideBar</h1>
+        </Fragment>
+    )
+}
